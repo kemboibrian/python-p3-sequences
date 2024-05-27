0d3e1f5b9c65a5036808d9c19e41f82cd2e09295
Brian Kemboi <brnkemboi@gmail.com>
2024-05-27 12:31:42 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length > 0:
+        fibonacci = [0]
+    if length > 1:
+        fibonacci.append(1)
+    while len(fibonacci) < length:
+        next_num = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_num)
+    print(fibonacci)
+
+# To test the function
+print_fibonacci(9)
+
